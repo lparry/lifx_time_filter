8c17a4951041f0e7bd3e10571228a2ef02ae5b9f
Lucas Parry <lparry@gmail.com>
2015-08-03 16:21:44 +1000

    turn on lights if past specified time

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 ruby "2.1.2"
 gem 'sinatra', '1.1.0'
 gem 'lifx_toys'
+gem 'activesupport'
 
--- web.rb
+++ web.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'lifx_toys'
+require 'active_support/time'
 
-get '/' do
-  "Hello, world"
+TIMEZONE = "Melbourne"
+
+# :time is in the format "6PM" or "6:15PM"
+put '/turn_on_lights_if_after/:time' do |specified_time|
+  if time_zone.now > time_zone.parse(specified_time)
+    api.set_power_state "on"
+  end
 end
+
+def lifx_api
+  LifxToys::HttpApi.with_default_selector("all")
+end
+
+def time_zone
+  ActiveSupport::TimeZone.new(TIMEZONE)
+end
+
