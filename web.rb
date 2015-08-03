83b9739ba5bbb860f0974d00e834229fb9097de0
Lucas Parry <lparry@gmail.com>
2015-08-03 16:46:43 +1000

    Blow up if we're missing anything important

--- web.rb
+++ web.rb
@@ -4,6 +4,13 @@ require 'active_support/time'
 
 TIMEZONE = "Melbourne"
 
+REQUIRED_ENV_SETTINGS = %w[
+                          HTTP_AUTH_USER
+                          HTTP_AUTH_PASSWORD
+                          LIFX_TOKEN
+                          ]
+
+
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   username == ENV['HTTP_AUTH_USER'] and password == ENV['HTTP_AUTH_PASSWORD']
 end
@@ -24,3 +31,11 @@ def time_zone
   ActiveSupport::TimeZone.new(TIMEZONE)
 end
 
+def check_required_environment
+  missing_env_vars = REQUIRED_ENV_SETTINGS.reject do |var|
+    ENV[var]
+  end
+  raise "Some required ENV vars are missing: #{missing_env_vars.join(", ")}" unless missing_env_vars.empty?
+end
+
+check_required_environment
