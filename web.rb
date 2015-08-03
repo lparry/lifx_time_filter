da7a7c18f376bf51b829c09bc5d4b66d6e40c884
Lucas Parry <lparry@gmail.com>
2015-08-03 16:24:13 +1000

    HTTP basic auth

--- web.rb
+++ web.rb
@@ -4,14 +4,19 @@ require 'active_support/time'
 
 TIMEZONE = "Melbourne"
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == ENV['HTTP_AUTH_USER'] and password == ENV['HTTP_AUTH_PASSWORD']
+end
+
 # :time is in the format "6PM" or "6:15PM"
 put '/turn_on_lights_if_after/:time' do |specified_time|
   if time_zone.now > time_zone.parse(specified_time)
-    api.set_power_state "on"
+    lights.set_power_state "on"
   end
+  ""
 end
 
-def lifx_api
+def lights
   LifxToys::HttpApi.with_default_selector("all")
 end
 
